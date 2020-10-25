a51d9d473245cb6029d0946cc8899246da0ab1ac
alex-masip <alexmasip@gmail.com>
2020-10-25 13:11:32 +0100

    Update run_once_install_packages.sh

--- run_once_install_packages.sh
+++ run_once_install_packages.sh
@@ -11,7 +11,7 @@ echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.
 sudo wget -P /usr/share/fonts/TTF 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf'
 sudo wget -P /usr/share/fonts/TTF 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf'
 sudo wget -P /usr/share/fonts/TTF 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf'
-sudo wget -P /usr/share/fonts/TTF 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Ialic.ttf'
+sudo wget -P /usr/share/fonts/TTF 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf'
 
 
 
